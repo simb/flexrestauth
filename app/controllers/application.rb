8a1995d0e8cdbe5a8c0bd9a778d89e313344da90
Simeon Bateman <simeon@simtop.local.domain>
2008-04-18 12:02:03 -0700

    adding rails application

---
+++ app/controllers/application.rb
@@ -0,0 +1,12 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '3c26683bc38fa2e69f9e7d43354b89dd'
+  
+  include AuthenticatedSystem
+end
